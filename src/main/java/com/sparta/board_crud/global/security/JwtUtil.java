a04a9c46ce3c10cb710082f362c399682515b44a
Yoon <dlstjr9390@gmail.com>
2023-12-12 20:59:48 +0900

    feat : JwtUtil 구현

---
+++ src/main/java/com/sparta/board_crud/global/security/JwtUtil.java
@@ -0,0 +1,118 @@
+package com.sparta.board_crud.global.security;
+
+import com.sparta.board_crud.user.entity.UserRoleEnum;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Slf4j
+@Component
+public class JwtUtil {
+    // Header KEY 값
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    // 사용자 권한 값의 KEY
+    public static final String AUTHORIZATION_KEY = "auth";
+    // Token 식별자
+    public static final String BEARER_PREFIX = "Bearer ";
+    // Token 만료 시간
+    private final long TOKEN_TIME = 60*60*1000L;
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    private Key key;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    public static final Logger logger = LoggerFactory.getLogger("JWT 관련 로그");
+
+    @PostConstruct
+    public void init(){
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String createToken(String username, UserRoleEnum role){
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(username)
+                        .claim(AUTHORIZATION_KEY,role)
+                        .setExpiration(new Date(date.getTime() + TOKEN_TIME))
+                        .setIssuedAt(date)
+                        .signWith(key,signatureAlgorithm)
+                        .compact();
+    }
+
+    public void addJwtToCookies(String token, HttpServletResponse response){
+        try{
+            token = URLEncoder.encode(token,"utf-8").replaceAll("\\+","%20");
+
+            Cookie cookie = new Cookie(AUTHORIZATION_HEADER,token);
+            cookie.setPath("/");
+
+            response.addCookie(cookie);
+        } catch (UnsupportedEncodingException e){
+            logger.error(e.getMessage());
+        }
+    }
+
+    public String substringToken(HttpServletRequest request){
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)){
+            return  bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token){
+
+        try{
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+        } catch (SecurityException | MalformedJwtException | SignatureException e) {
+            logger.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+        } catch (ExpiredJwtException e) {
+            logger.error("Expired JWT token, 만료된 JWT token 입니다.");
+        } catch (UnsupportedJwtException e) {
+            logger.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+        }
+        return false;
+    }
+
+    public Claims getUserInfoFromToken(String token){
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    public String getTokenFromRequest(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for(Cookie cookie : cookies){
+                if(cookie.getName().equals(AUTHORIZATION_HEADER)){
+                    try {
+                        return URLDecoder.decode(cookie.getValue(),"UTF-8");
+                    } catch (UnsupportedEncodingException e){
+                        return null;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
