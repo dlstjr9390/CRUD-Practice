0b64b375c1a39536ef99c0542b61547af0ef830f
Yoon <dlstjr9390@gmail.com>
2023-12-12 21:03:16 +0900

    feat : Timestamp

---
+++ src/main/java/com/sparta/board_crud/global/common/Timestamp.java
@@ -0,0 +1,26 @@
+package com.sparta.board_crud.global.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamp {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+
+}
