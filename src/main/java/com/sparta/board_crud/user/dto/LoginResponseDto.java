b9bc4e47e916a114d6ef6c3b6d287422c67020ad
Yoon <dlstjr9390@gmail.com>
2023-12-13 21:46:10 +0900

    feat : message와 code 반환

--- src/main/java/com/sparta/board_crud/user/dto/LoginResponseDto.java
+++ src/main/java/com/sparta/board_crud/user/dto/LoginResponseDto.java
@@ -2,14 +2,20 @@ package com.sparta.board_crud.user.dto;
 
 import com.sparta.board_crud.user.entity.User;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class LoginResponseDto {
-    private String username;
+    private HttpStatus code;
     private String msg;
 
-    public LoginResponseDto(User user){
-        this.username = user.getUsername();
-        this.msg = username + "님 환영합니다.";
+    public LoginResponseDto(User user, HttpStatus code){
+        this.code = code;
+        this.msg = user.getUsername() + "님 환영합니다.";
+    }
+
+    public LoginResponseDto(String errorMessage,HttpStatus code){
+        this.code = code;
+        this.msg = errorMessage;
     }
 }
