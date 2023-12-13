ce273dcba52016eb20aeee6e144d1aa8b42a0557
Yoon <dlstjr9390@gmail.com>
2023-12-13 21:45:27 +0900

    feat: 유효성 검사 추가

--- src/main/java/com/sparta/board_crud/user/dto/LoginRequestDto.java
+++ src/main/java/com/sparta/board_crud/user/dto/LoginRequestDto.java
@@ -1,9 +1,15 @@
 package com.sparta.board_crud.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class LoginRequestDto {
-    private String email;
+    @NotBlank(message = "아이디를 입력해주세요.")
+    private String username;
+
+    @NotBlank(message = "비밀번호를 입력해주세요")
     private String password;
+
 }
--- src/main/java/com/sparta/board_crud/user/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/board_crud/user/dto/SignupRequestDto.java
@@ -1,10 +1,28 @@
 package com.sparta.board_crud.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import org.springframework.security.core.parameters.P;
 
 @Getter
 public class SignupRequestDto {
+    @NotBlank(message = "아이디를 입력해주세요.")
+    @Pattern(regexp = "^[0-9A-Za-z]*$", message = "아이디는 8~16자 영어 소문자와 숫자만 입력 가능합니다.")
+    @Size(min =3, max = 16)
     private String username;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(regexp = "^[!-~]*$",
+            message = "비밀번호는 8~16자 영어 대 소문자, 숫자, 특수문자만 입력 가능합니다.")
+    @Size(min =4, max = 16)
     private String password;
+
+    @NotBlank(message = "비밀번호 확인을 입력해주세요.")
+    private String checkedPassword;
+
+    @Email(message = "옳지 않은 형식 입니다.")
     private String email;
 }
