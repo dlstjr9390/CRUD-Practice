35c0f3e99435edbb335bd9f2c793617845958ec6
Yoon <dlstjr9390@gmail.com>
2023-12-14 20:52:22 +0900

    refactor : 사용자 인증이 필요한 요청 추가

--- src/main/java/com/sparta/board_crud/global/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/board_crud/global/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -42,6 +43,7 @@ public class WebSecurityConfig {
         httpSecurity.authorizeHttpRequests((authorizeHttpRequests)->
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/posts/").authenticated()
                         .anyRequest().permitAll()
 
 
