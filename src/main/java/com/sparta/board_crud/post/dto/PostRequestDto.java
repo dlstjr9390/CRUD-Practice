e16d3171cc6d68e9a7a4bf25dc4b33aa8e5326b1
Yoon <dlstjr9390@gmail.com>
2023-12-18 21:33:02 +0900

    feat : 제목 최대 길이 수정

--- src/main/java/com/sparta/board_crud/post/dto/PostRequestDto.java
+++ src/main/java/com/sparta/board_crud/post/dto/PostRequestDto.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 public class PostRequestDto {
 
     @NotBlank(message = "제목을 입력해주세요.")
-    @Size(min = 1, max = 5, message = "제목의 길이는 1에서 500 사이여야 합니다.")
+    @Size(min = 1, max = 500, message = "제목의 길이는 1에서 500 사이여야 합니다.")
     private String title;
 
     @NotBlank(message = "내용을 입력해주세요")
