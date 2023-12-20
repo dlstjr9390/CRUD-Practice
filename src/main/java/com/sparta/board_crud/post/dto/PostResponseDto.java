3853da1c489ecd3ecebedc3b35db82c690a5bc39
Yoon <dlstjr9390@gmail.com>
2023-12-20 21:45:38 +0900

    feat : 게시물 조회에 댓글 조회 추가

--- src/main/java/com/sparta/board_crud/post/dto/PostResponseDto.java
+++ src/main/java/com/sparta/board_crud/post/dto/PostResponseDto.java
@@ -1,9 +1,12 @@
 package com.sparta.board_crud.post.dto;
 
+import com.sparta.board_crud.comment.dto.CommentResponseDto;
+import com.sparta.board_crud.comment.entity.Comment;
 import com.sparta.board_crud.post.entity.Post;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 public class PostResponseDto {
@@ -11,11 +14,14 @@ public class PostResponseDto {
     private String username;
     private String content;
     private LocalDateTime createdAt;
+    private List<CommentResponseDto> commentList;
 
-    public PostResponseDto(Post post) {
+    public PostResponseDto(Post post,List<CommentResponseDto> commentResponseDtoList) {
         this.title = post.getTitle();
         this.username = post.getUser().getUsername();
         this.content = post.getContent();
         this.createdAt = post.getCreatedAt();
+        this.commentList = commentResponseDtoList;
     }
+
 }
